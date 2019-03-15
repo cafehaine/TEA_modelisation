59c98d626f2b3f8b279c0701bf8ac418ee683c2f
Guillaume Kilian <kguillau@u18-bionic-0614.univ-lr.fr>
2019-03-15 09:10:33 +0100

    Add systeme

---
+++ Systeme/src/Systeme.java
@@ -0,0 +1,24 @@
+
+public class Systeme {
+	private BaseDeDonnees bdd;
+	private ServeurMail servMail;
+
+	public Systeme(BaseDeDonnees bdd, ServeurMail servMail) {
+		this.bdd = bdd;
+		this.servMail = servMail;
+	}
+	
+	public void creerRendezVous(RendezVous rdv) {
+		int idRdv = bdd.rendezVous.creerRendezVous(rdv);
+		for (Integer user : rdv.getParticipants()) {
+			boolean dispo = bdd.utilisateur.utilisateurDisponnible(user, idRdv);
+			if (dispo) {
+				servMail.demanderConfirm(user, idRdv);
+			}
+		}
+	}
+	
+	public void retourConfirm(int utilisateur, int rdv, boolean accepte, int agenda) {
+		;
+	}
+}

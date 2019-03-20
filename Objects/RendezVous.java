ef5a69cf81756e98ecd23a46359a004a48663123
cafehaine <kilian.guillaume@gmail.com>
2019-03-20 16:53:15 +0100

    Reformat RendezVous

--- Objects/RendezVous.java
+++ Objects/RendezVous.java
@@ -5,23 +5,21 @@ import java.util.Calendar;
 
 public class RendezVous {
 	private static int nbId = 0;
-	
+
 	private int id;
 	private Calendar date;
 	private String titre;
 	private Double duree; // 1h30 = 1.5
 	private ArrayList<Integer> lstParticipant;
 	private ArrayList<Integer> lstConfirmer;
-	
+
 	public RendezVous(Calendar date, String titre, Double duree) {
-		super();
 		this.id = ++nbId;
 		this.date = date;
 		this.titre = titre;
 		this.duree = duree;
 		this.lstParticipant = new ArrayList<Integer>();
 		this.lstConfirmer = new ArrayList<Integer>();
-				
 	}
 
 	public int getId() {
@@ -59,22 +57,22 @@ public class RendezVous {
 	public ArrayList<Integer> getLstparticipant() {
 		return lstParticipant;
 	}
-	
+
 	public ArrayList<Integer> getLstConfirmer() {
 		return lstConfirmer;
 	}
-	
+
 	public boolean addParticipant(int idUtilisateur) {
 		return this.lstParticipant.add(idUtilisateur);
 	}
-	
+
 	public boolean addConfirmer(int idUtilisateur) {
 		return this.lstConfirmer.add(idUtilisateur);
 	}
-	
+
 	public String toString() {
 		String ch = "Rdv : "+this.titre+" date (mois) : "+this.date.get(Calendar.LONG_FORMAT)+" durée : "+this.duree;
 		return ch;
 	}
-	
+
 }

b9581420c0752af6bd206e52772442437f125454
cafehaine <kilian.guillaume@gmail.com>
2019-03-20 16:49:46 +0100

    Simplify ServeurMail

--- ServeurMail/ServeurMail.java
+++ ServeurMail/ServeurMail.java
@@ -9,51 +9,44 @@ public class ServeurMail {
 
 	private BaseDeDonnees bdd;
 	private SystemeInterface system;
-	
+
 	public ServeurMail() {
 		this.bdd = new BaseDeDonnees();
 		this.system = null;
 	}
-	
+
 	public void setSystem(SystemeInterface sys) {
 		this.system = sys;
 	}
 
 	public void demanderConfirm(int utilisateur, int rdv) {
 		ArrayList<Integer> liste_Agenda = bdd.utilisateur.listeAgenda(utilisateur);
-	
+
 		//ArrayList<Agenda> mesAgendas = idAgendaToAgenda(liste_Agenda); 
-		int idAgenda = agendaAlea(liste_Agenda.size());		
+		int idAgenda = agendaAlea(liste_Agenda.size());
 		boolean accepte = booleanAlea();
-	
+
 		system.retourConfirm(utilisateur,rdv,accepte,idAgenda);
-			
+
 	}
-	
-	/*public ArrayList<Agenda> idAgendaToAgenda(ArrayList<Integer> listeIdAgenda){
+
+	/*
+	public ArrayList<Agenda> idAgendaToAgenda(ArrayList<Integer> listeIdAgenda){
 		ArrayList <Agenda> lstAgenda = new ArrayList<Agenda>();
 		for (int i=0;i<listeIdAgenda.size();i++) {
 			lstAgenda.add(bdd.getAgenda(liste_Agenda.get(i)));
 		}
 		return lstAgenda;
-	}*/
-	
+	}
+	*/
+
 	public int agendaAlea(int nb) {
-		int idAgenda = (int) (Math.random()*(nb-1)) + 1;		
+		int idAgenda = (int)(Math.random()*(nb-1)) + 1;
 		return idAgenda;
 	}
-	
+
 	public boolean booleanAlea() {
-		boolean rep = false;
-		
-		int bool = (int) (Math.random()*(1-0)) + 0;		
-		
-		if (bool == 1) {
-			rep = true;
-		}
-		
-		return rep;
-	
+		int valbool = (int)(Math.random()*(1-0)) + 0;
+		return valbool == 1;
 	}
-	
 }
